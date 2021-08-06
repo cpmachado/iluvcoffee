58deb5a0abfccf465a2b611b5b3bf16d2fd3aabb
cpmachado <cpmachado@protonmail.com>
2021-08-06 14:56:55 +0100

    Chapter 2: Response Status Codes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,18 +1,29 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll(): string {
     return 'This action returns all coffees';
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string): string {
     return `This action returns #${id} coffee`;
   }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() body: any): any {
     return body;
   }
