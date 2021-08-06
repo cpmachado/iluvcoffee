aec16e20c4c60435c8e9e0256cd7b0fa364f0791
cpmachado <cpmachado@protonmail.com>
2021-08-06 15:15:37 +0100

    Chapter 2: Handling Update and Delete Requests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 
@@ -26,5 +28,17 @@ export class CoffeesController {
   @HttpCode(HttpStatus.CREATED)
   create(@Body() body: any): any {
     return body;
+  }
+
+  @Patch(':id')
+  @HttpCode(HttpStatus.OK)
+  update(@Param('id') id: string, @Body() body: any): string {
+    return `This action updates #${id} coffee with '${JSON.stringify(body)}'`;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  remove(@Param('id') id: string): string {
+    return `This action removes #${id} coffee`;
   }
 }
