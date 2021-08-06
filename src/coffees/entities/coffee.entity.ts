3dff7d14f6c7933d163708520249f80cc7e0857f
cpmachado <cpmachado@protonmail.com>
2021-08-07 00:18:49 +0100

    Chapter 3: Creating a TypeORM Entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   name: string;
+  @Column()
   brand: string;
+  @Column('json', { nullable: true })
   flavors: string[];
 }
